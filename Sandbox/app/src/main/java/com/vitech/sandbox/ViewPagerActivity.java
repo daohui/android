a4b6b303d65291d89d357154c4543ee91410d75b
cdl01c <cdl01c@motorolasolutions.com>
2016-10-25 18:44:14 -0500

    add tabLayout for ViewPager
    Core-Id: CDL01C

--- Sandbox/app/src/main/java/com/vitech/sandbox/ViewPagerActivity.java
+++ Sandbox/app/src/main/java/com/vitech/sandbox/ViewPagerActivity.java
@@ -2,6 +2,7 @@ package com.vitech.sandbox;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
@@ -36,6 +37,8 @@ public class ViewPagerActivity extends AppCompatActivity {
                     }
                 }
         );
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.viewpager_tab);
+        tabLayout.setupWithViewPager(pager);
     }
 
     @Override
